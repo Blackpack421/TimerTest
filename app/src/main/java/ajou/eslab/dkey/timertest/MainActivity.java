f4899d1fc5c05a61f5077ea8a96ed3e548a2fc6c
Kim Deok Ki <kdk0421@gmail.com>
2015-10-03 19:16:56 +0900

    Initial commit

---
+++ app/src/main/java/ajou/eslab/dkey/timertest/MainActivity.java
@@ -0,0 +1,88 @@
+package ajou.eslab.dkey.timertest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
+
+    Timer mTimer = new Timer();
+    TextView tvHello;
+    Button btnCancel, btnResume;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvHello = (TextView) findViewById(R.id.tvHello);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnResume = (Button) findViewById(R.id.btnResume);
+
+        btnCancel.setOnClickListener(mOnClickListener);
+        btnResume.setOnClickListener(mOnClickListener);
+
+        mTimer.schedule(testTimerTask, 10000, 10000);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    TimerTask testTimerTask = new TimerTask() {
+        @Override
+        public void run() {
+            //tvHello.setText("TIMER ACT!");
+            Log.d("TEST.DKEY", "TIMER ACTIVE !!!");
+        }
+    };
+
+    Button.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()) {
+                case R.id.btnCancel: {
+                    testTimerTask.cancel();
+                    Log.d("TEST.DKEY", "timertask cancel()");
+                    //mTimer.cancel();
+                    //Log.d("TEST.DKEY", "timer cancel()");
+                    mTimer.purge();
+                    Log.d("TEST.DKEY", "timer purge() canceled task");
+                    break;
+                }
+                case R.id.btnResume: {
+                    mTimer.schedule(testTimerTask, 10000, 10000);
+                    Log.d("TEST.DKEY", "resume timertask! [Timer.schedule]");
+                    break;
+                }
+            }
+        }
+    };
+}
